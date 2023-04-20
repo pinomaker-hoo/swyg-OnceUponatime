d709ab49c4bf895f9806cfb1eff75ccde1daa899
pinomaker-hoo <inhoo987654321@gmail.com>
2023-04-20 16:35:22 +0900

    feat : Set Mui Config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
 // ** Next Imports
 import type { AppProps } from 'next/app'
 
+// ** Theme Imports
+import { ThemeProvider } from '@mui/material/styles'
+import { GlobalStyles } from '@mui/material'
+import CssBaseline from '@mui/material/CssBaseline'
+import theme from 'theme'
+import 'styles/globals.css'
+
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles styles={{ backgroundColor: 'blue' }} />
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default App
