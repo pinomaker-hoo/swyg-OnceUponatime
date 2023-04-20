de361503aa8f9fea1003ffe218bc564f956a3f57
pinomaker-hoo <inhoo987654321@gmail.com>
2023-04-20 16:30:50 +0900

    feat : Add Meta Script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <title>한 줄의 이야기</title>
+        <meta
+          name="description"
+          content="한 줄의 이야기를 남겨서 남에게 보내부세요."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
