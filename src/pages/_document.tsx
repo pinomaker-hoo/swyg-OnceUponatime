90130fdce79d2b96ce692ac9782e1b1ead03de5c
pinomaker-hoo <inhoo987654321@gmail.com>
2023-04-20 16:27:03 +0900

    feat : Init Code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,9 @@
+// ** Next Imports
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ko">
       <Head />
       <body>
         <Main />
