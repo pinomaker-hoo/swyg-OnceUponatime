2a8c82eaf5e2fbaa084c097f943b461e2f6a4c6b
pinomaker-hoo <inhoo987654321@gmail.com>
2023-04-20 16:41:32 +0900

    feat : Set Redux Tool Kit Query

--- src/store/index.ts
+++ src/store/index.ts
@@ -14,6 +14,7 @@ import storage from 'redux-persist/lib/storage'
 
 // ** Reducer Imports
 import auth from './auth'
+import { api } from 'services'
 
 // ** Other Imports
 import { middleware } from './middleware'
@@ -22,9 +23,11 @@ const persistConfig = {
   key: 'root',
   version: 1,
   storage,
+  blacklist: [api.reducerPath],
 }
 
 const reducer = combineReducers({
+  [api.reducerPath]: api.reducer,
   auth,
 })
 
