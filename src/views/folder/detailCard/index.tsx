5f523b4e69697c29f2de3ff6a95d5a8ca3724ed7
pinomaker-hoo <56928532+pinomaker-hoo@users.noreply.github.com>
2023-04-29 15:56:19 +0900

    fix : Card View

--- src/views/folder/detailCard/index.tsx
+++ src/views/folder/detailCard/index.tsx
@@ -1,4 +1,4 @@
-import { Grid, Typography, Button, Chip } from '@mui/material'
+import { Grid, Typography, Button, Chip, Paper } from '@mui/material'
 import Link from 'next/link'
 
 interface DetailPageViewProps {
@@ -13,7 +13,7 @@ const DetailPageView = ({ title, tag, text, img, id }: DetailPageViewProps) => {
   return (
     <Grid container>
       <Grid item xs={1} />
-      <Grid item xs={10} sx={{ mt: 10 }}>
+      <Grid item xs={10} sx={{ mt: 3 }}>
         <Grid container>
           <Grid item xs={2}>
             <img src="/back.png" />
@@ -26,29 +26,41 @@ const DetailPageView = ({ title, tag, text, img, id }: DetailPageViewProps) => {
               <img src="/check.png" />
             </Button>
           </Grid>
-          <Grid item xs={12} sx={{ textAlign: 'center' }}>
-            <img src={img} />
+          <Grid item xs={12} sx={{ textAlign: 'center', ml: 2, my: 5 }}>
+            <Paper
+              sx={{
+                boxShadow: 'none',
+                border: 1,
+                borderRadius: 2,
+                width: 300,
+                height: 300,
+                textAlign: 'center',
+                backgroundColor: '#f2f2f2',
+              }}
+            >
+              <img src={img} width="100%" />
+            </Paper>
           </Grid>
           {tag.map((item: string, index: number) => (
             <Grid item xs={4} key={index}>
               <Chip label={item} />
             </Grid>
           ))}
-          <Grid item xs={12}>
+          <Grid item xs={12} sx={{ my: 5 }}>
             <Typography>{text}</Typography>
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={6} sx={{ textAlign: 'center' }}>
             <Link href={`/folder/edit/${id}`}>
               <img src="/edit.png" />
             </Link>
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={6} sx={{ textAlign: 'center' }}>
             <img src="/save.png" />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={6} sx={{ textAlign: 'center' }}>
             <Typography>수정하기</Typography>
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={6} sx={{ textAlign: 'center' }}>
             <Typography>저장</Typography>
           </Grid>
         </Grid>
