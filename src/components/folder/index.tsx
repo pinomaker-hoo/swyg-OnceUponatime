76aba42552845f4063ce66430398fa87bad6306a
pinomaker-hoo <56928532+pinomaker-hoo@users.noreply.github.com>
2023-04-29 11:43:57 +0900

    feat : Folder Component

---
+++ src/components/folder/index.tsx
@@ -0,0 +1,49 @@
+// ** Mui Import
+import { Grid, Typography } from '@mui/material'
+
+interface FolderProps {
+  title: string
+  count: number
+}
+
+const Folder = ({ title, count }: FolderProps) => {
+  return (
+    <Grid container>
+      <Grid item xs={12} sx={{ textAlign: 'center' }}>
+        <img src="/folder.png" />
+      </Grid>
+      <Grid container spacing={3} sx={{ position: 'relative' }}>
+        <Grid
+          item
+          xs={12}
+          sx={{
+            position: 'absolute',
+            left: '50%',
+            top: '-200%',
+            transform: 'translate(-50%, -50%)',
+          }}
+        >
+          <Typography variant="h6" sx={{ fontSize: 19 }}>
+            {title}
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sx={{
+            position: 'absolute',
+            left: '50%',
+            top: '-70%',
+            transform: 'translate(-50%, -50%)',
+          }}
+        >
+          <Typography variant="body1" sx={{ fontSize: 15 }}>
+            {count}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Folder
