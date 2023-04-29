50ca880a8e145b3aa22f31e85053f80d778509f8
pinomaker-hoo <56928532+pinomaker-hoo@users.noreply.github.com>
2023-04-29 15:51:48 +0900

    fix : Write Card View

--- src/views/folder/write/index.tsx
+++ src/views/folder/write/index.tsx
@@ -1,38 +1,41 @@
 // ** Mui Imports
 import { Grid, Typography, Button, Paper, TextField } from '@mui/material'
+import Link from 'next/link'
 
 const CardWritePageView = () => {
   return (
     <Grid container>
       <Grid item xs={1} />
-      <Grid item xs={10} sx={{ mt: 10 }}>
+      <Grid item xs={10} sx={{ mt: 3 }}>
         <Grid container>
           <Grid item xs={2}>
-            <img src="/back.png" />
+            <Link href="/folder/0/list">
+              <img src="/back.png" />
+            </Link>
           </Grid>
           <Grid item xs={10} sx={{ textAlign: 'right' }}>
             <Button sx={{ p: 0 }}>
               <Typography>완료</Typography>
             </Button>
           </Grid>
-          <Grid item xs={1} />
-          <Grid item xs={10}>
+          <Grid item xs={12} sx={{ ml: 2, mt: 5 }}>
             <Button sx={{ p: 0 }}>
               <Paper
                 sx={{
                   boxShadow: 'none',
                   border: 1,
-                  width: 250,
-                  height: 250,
+                  borderRadius: 2,
+                  width: 300,
+                  height: 300,
                   textAlign: 'center',
+                  backgroundColor: '#f2f2f2',
                 }}
               >
                 <img src="/plus.png" />
               </Paper>
             </Button>
           </Grid>
-          <Grid item xs={1} />
-          <Grid item xs={12} sx={{ textAlign: 'center' }}>
+          <Grid item xs={12} sx={{ textAlign: 'center', mt: 3 }}>
             <TextField variant="standard" sx={{ width: '80%' }} label="제목" />
           </Grid>
           <Grid item xs={12} sx={{ textAlign: 'center', my: 1 }}>
