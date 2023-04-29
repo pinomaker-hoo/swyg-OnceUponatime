952becab67e200921996c416ef9f85e022fa68fd
pinomaker-hoo <56928532+pinomaker-hoo@users.noreply.github.com>
2023-04-29 12:08:06 +0900

    feat : Add Folder

---
+++ src/components/addFolder/index.tsx
@@ -0,0 +1,28 @@
+// ** Mui Import
+import { Grid } from '@mui/material'
+
+const AddFolder = () => {
+  return (
+    <Grid container>
+      <Grid item xs={12} sx={{ textAlign: 'center' }}>
+        <img src="/folder.png" />
+      </Grid>
+      <Grid container spacing={3} sx={{ position: 'relative' }}>
+        <Grid
+          item
+          xs={12}
+          sx={{
+            position: 'absolute',
+            left: '50%',
+            top: '-175%',
+            transform: 'translate(-50%, -50%)',
+          }}
+        >
+          <img src="/plus.png" />
+        </Grid>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default AddFolder
