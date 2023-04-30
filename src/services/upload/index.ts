44aa99a3f10fa649e32fc187bb1e92a1d92949a4
pinomaker-hoo <56928532+pinomaker-hoo@users.noreply.github.com>
2023-05-01 03:22:52 +0900

    fix : Uplaod Api

--- src/services/upload/index.ts
+++ src/services/upload/index.ts
@@ -5,12 +5,8 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage'
 const upload = {
   imgUpload: async (image: any) => {
     const imageRef = ref(storage, `images/${image.name}`)
-
-    uploadBytes(imageRef, image).then((snapshot) => {
-      getDownloadURL(snapshot.ref).then((url) => {
-        return url
-      })
-    })
+    const snapshot = await uploadBytes(imageRef, image)
+    return await getDownloadURL(snapshot.ref)
   },
 }
 
